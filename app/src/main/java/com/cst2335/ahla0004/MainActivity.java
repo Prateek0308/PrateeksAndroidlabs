84dbb8984a591d181e5b86b3db650fdc152b5067
Prateek0308 <ahla0004@algonquinlive.com>
2021-07-18 14:37:23 -0400

    Finished step 2

--- app/src/main/java/com/cst2335/ahla0004/MainActivity.java
+++ app/src/main/java/com/cst2335/ahla0004/MainActivity.java
@@ -3,6 +3,8 @@ package com.cst2335.ahla0004;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hello");
+
+
     }
 }
